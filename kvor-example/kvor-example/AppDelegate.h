ee8f55e2e495fa2ec6fa24f07172ec517136de44
hellozimi <hello@zimi.se>
2013-02-23 21:57:07 +0100

    added xcode project

---
+++ kvor-example/kvor-example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  kvor-example
+//
+//  Created by Simon Andersson on 2/23/13.
+//  Copyright (c) 2013 hiddencode.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
