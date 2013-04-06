bfd3f3c4464578551b062516679ca3beac8d1a3a
hellozimi <hello@zimi.se>
2013-04-06 18:19:01 +0200

    implemented hash into Constants

--- KVOR/KVORConstants.h
+++ KVOR/KVORConstants.h
@@ -6,8 +6,12 @@
 //  Copyright (c) 2013 hiddencode.me. All rights reserved.
 //
 
-#ifndef kvor_example_KVORTaskBlock_h
-#define kvor_example_KVORTaskBlock_h
+#ifndef kvor_example_KVORConstants_h
+#define kvor_example_KVORConstants_h
+
+static inline uint KVORHashPointerForObserver(id object, NSString *keyPath) {
+    return [[NSString stringWithFormat:@"%p.%@.%@", object, NSStringFromClass([object class]), keyPath] hash];
+}
 
 typedef void(^KVORObserverTaskBlock)(NSString *keyPath, NSDictionary *change);
 
