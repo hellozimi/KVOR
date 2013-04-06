81a604888bff39a32f7ae44009bd136d92b803ad
hellozimi <hello@zimi.se>
2013-04-07 00:20:35 +0200

    added documentation

--- KVOR/KVOR.h
+++ KVOR/KVOR.h
@@ -11,11 +11,53 @@
 
 @interface KVOR : NSObject
 
+/**
+ * Adds a key-value observation with a single key path
+ * 
+ * @param target - The object which is changing values
+ * @param keyPath - The key path you want changes for
+ * @param block - The change block which is triggered on change
+ *
+ * @returns void
+ */
 + (void)target:(id)target keyPath:(NSString *)keyPath task:(KVORObserverTaskBlock)block;
+
+/**
+ * Adds a key-value observation with a single key path
+ *
+ * @param target - The object which is changing values
+ * @param keyPatha - An array with the key paths you want changes for
+ * @param block - The change block which is triggered on change
+ *
+ * @returns void
+ */
 + (void)target:(id)target keyPaths:(NSArray *)keyPaths task:(KVORObserverTaskBlock)block;
 
-+ (void)removeAllObservers;
+/**
+ * Removes an already existing observer
+ *
+ * @param target - The object which is changing values
+ * @param keyPath - The key path which you've been listening for changes
+ *
+ * @returns void
+ */
 + (void)removeObserverWithTarget:(id)target andKeyPath:(NSString *)keyPath;
+
+/**
+ * Removes an already existing observer
+ *
+ * @param target - The object which is changing values
+ * @param keyPaths - The key paths which you've been listening for changes
+ *
+ * @returns void
+ */
 + (void)removeObserverWithTarget:(id)target andKeyPaths:(NSArray *)keyPaths;
+
+/**
+ * Removes all existing KVOR observers
+ *
+ * @returns void
+ */
++ (void)removeAllObservers;
 
 @end
